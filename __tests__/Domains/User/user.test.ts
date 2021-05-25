9d267e98e19f4d4f68e2794864adae51fcf93a4b
Eduardo Ferreira <eduardoferreira@wine.com.br>
2021-05-25 19:11:22 -0300

    Fix: linkt

--- __tests__/Domains/User/user.test.ts
+++ __tests__/Domains/User/user.test.ts
@@ -1,7 +1,7 @@
-import {User} from '../../../src/core/models/domain/User/User';
+import { User } from '../../../src/core/models/domain/User/User';
 describe('User test', () => {
   it('should be able to create an user', () => {
-    const user = new User({name: 'Jhon Doe', email: 'jhon.doe@doe.com'});
+    const user = new User({ name: 'Jhon Doe', email: 'jhon.doe@doe.com' });
 
     expect(user.getName()).toEqual('Jhon Doe');
     expect(user.getEmail()).toEqual('jhon.doe@doe.com');
